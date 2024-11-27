297ea59d30f017a62be0051e043959df881a77d6
Daniel Souza <danielsouzaaj@hotmail.com>
2024-11-26 22:45:49 -0300

    feat: Criando page home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material"
+
+function Home() {
+    return (
+        <div>
+            <Typography>Home</Typography>
+        </div>
+    )
+}
+
+export default Home
