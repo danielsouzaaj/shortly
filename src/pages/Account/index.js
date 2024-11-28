e96349ee2cec8f44918a998f759c8ab30ffe5ffb
Daniel Souza <danielsouzaaj@hotmail.com>
2024-11-27 23:54:58 -0300

    feat: adicionando links state e data list

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,17 +1,34 @@
 import { Grid2, Box, Typography, Button } from "@mui/material"
 import Navbar from "./Navbar"
+import { useState } from "react"
+import LinkCard from "./LinkCard"
+
+const data = [
+    {
+        id: "0",
+        createdAt: new Date(),
+        name: "My website",
+        longURL: "https://google.com",
+        shortCode: "masdo",
+        totalClicks: 10,
+    }
+]
 
 function Account() {
+    const [links, setLinks] = useState(data)
+
     return (
         <>
             <Navbar />
             <Box mt={5}>
                 <Grid2 container justifyContent="center">
                     <Grid2 size={{xs: 8}}>
-                        <Box display="flex" columnGap={3}>
+                        <Box mb={5} display="flex" columnGap={3}>
                             <Typography variant="h4">Links</Typography>
                             <Button variant="contained" color="primary">Create new</Button>
                         </Box>
+
+                        {links.map(link => <LinkCard key={link.id} {...link} />)}
                     </Grid2>
                 </Grid2>
             </Box>
